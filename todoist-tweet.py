607ccaf2ff7dc7d8110418bf88de754af5892994
Dan Bailey <dbailey@danbailey.net>
2023-04-05 11:48:39 -0500

    almost done...

--- todoist-tweet.py
+++ todoist-tweet.py
@@ -6,28 +6,28 @@ import tweepy
 from datetime import datetime, timedelta
 import pytz
 
-# set this to your local time zone
-localTimezone = "America/Chicago"
-
 # get the .env variables
 load_dotenv()
+
+# set this to your local time zone
+localTimezone = "America/Chicago"
 
 # set formats for datetime
 dateFormatFull = "%Y-%m-%d %H:%M:%S"
 dateFormatShort = "%Y-%m-%d"
 
-# calculate local difference from UTC time
-TIMEDIFF = datetime.utcnow().hour - datetime.now().hour
-print (datetime.utcnow().hour)
-print (datetime.now().hour)
-print ("Timediff = " + str(TIMEDIFF))
+# nail down what today's local date is
 TODAY = datetime.now().strftime(dateFormatShort)
-print (TODAY)
+print(TODAY)
 
-# set up a function to change the date from GMT to local, returns a datetime object
-def LocalizeTime(UTC, difference):
-    out = UTC - timedelta(hours=difference)
-    return out
+# functions for shifting to local time
+def makeSmartUTCObject(dumbDTObject):
+    smartDT = dumbDTObject.replace(tzinfo=pytz.utc)
+    return smartDT
+
+def convertToLocalTime(smartDTObject):
+    kickback = smartDTObject.astimezone(pytz.timezone(localTimezone))
+    return kickback
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(os.environ['TWITTER_CONSUMER_KEY'], os.environ['TWITTER_CONSUMER_SECRET'])
@@ -37,13 +37,15 @@ auth.set_access_token(os.environ['TWITTER_ACCESS_TOKEN'], os.environ['TWITTER_AC
 api = tweepy.API(auth)
 
 # set up a function to return the ID of the authenticated user's most-recent tweet, for building a whole thread of tweets, if need be
+def postTweet(tw):
+    api.update_status(tw)
 
 def mostRecentTweetID():
     timeline = api.user_timeline()
     return timeline[0].id
 
-def postAsReply(statusTweet):
-    api.update_status(status = statusTweet, in_reply_to_status_id = mostRecentTweetID(), auto_populate_reply_metadata=True)
+def postAsReply(statusTweet, RID):
+    api.update_status(status = statusTweet, in_reply_to_status_id = RID, auto_populate_reply_metadata=True)
 
 
 
@@ -58,6 +60,8 @@ jsonPackage = response.json()
 # filter down to just the items list, nothing else needed
 todos = jsonPackage['items']
 
+
+
 # prep the list for execution, count elements
 for todo in todos:
     # clean up formatting on todoist's time entry, below
@@ -65,10 +69,12 @@ for todo in todos:
     todo["completed_at"] = todo["completed_at"].replace(".000000Z", "")
     # convert the completed_at value to a local datetime object, below
     dtObject = datetime.strptime(todo["completed_at"], dateFormatFull)
-    dtObject = LocalizeTime(dtObject, TIMEDIFF)
+    dtObject = makeSmartUTCObject(dtObject)
+    localizedTime = convertToLocalTime(dtObject)
     #store the new values
-    todo["completed_at_local"] = dtObject.strftime(dateFormatFull)
-    todo["completed_local_date"] = dtObject.strftime(dateFormatShort)
+    todo["completed_at_local"] = localizedTime.strftime(dateFormatFull)
+    todo["completed_local_date"] = localizedTime.strftime(dateFormatShort)
+
 
 # make a list of things that were completed today, in order
 results = []
@@ -84,11 +90,13 @@ listTitle = str(x) + " items completed today:\n"
 results.insert(0, [listTitle, len(listTitle)])
 
 
-# character count
+
+# variables for building tweets
 tweetArray = []
 charCount = 0
 tweetTemp = ""
 
+# build the tweets
 for item in results:
     if (charCount + item[1]) < 288:
         charCount = charCount + item[1]
@@ -98,9 +106,22 @@ for item in results:
         tweetArray.append(tweetTemp)
         charCount = 0
         tweetTemp = ""
+# push the last bit into the tweet array
+if (len(tweetTemp) > 0):
+    tweetArray.append(tweetTemp)
+    tweetTemp = ""
 
-#print (len(tweetArray))
-#print (tweetArray[0])
-#print (tweetArray[1])
 
-# print(todos)
+
+# publish the chain of stuff
+maxlength = len(tweetArray)
+print(maxlength)
+count = 1
+if (len(tweetArray) >= 1):
+    # postTweet(tweetArray[0])
+    print(tweetArray[0])
+    while (count < maxlength):
+        # postAsReply(tweetArray[count], mostRecentTweetID())
+        print(" ")
+        print(tweetArray[count])
+        count += 1
